9ea8cd96808bc25437b2b584809ff75b6872f422
Cameron Sachse <cam.sachse1@gmail.com>
2021-09-28 19:53:05 -0400

    Final commit running way behind where i wanted

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 $("#search").on('click', function() {
     var city = $('#city').val()
     getCity(city);
+    var tempLS = JSON.parse(localStorage.getItem('searchHistory'));
+    if (tempLS == null) {
+        tempLS = [];
+    }
+    tempLS.unshift(city);
+    if (tempLS.length >= 8) {
+        tempLS.pop()
+    }
+    localStorage.setItem('searchHistory', JSON.stringify(tempLS));
+    var list = document.querySelector('#buttonList');
+    list.innerHTML = '';
+    getButtons();
 })
+getButtons();
+function getButtons() {
+    var list = document.querySelector('#buttonList');
+    // list.removeChild();
+    var tempLS = JSON.parse(localStorage.getItem('searchHistory'));
+    for (var i = 0; i < tempLS.length; i++) {
+        var temp = tempLS[i];
+        var tempB = document.createElement('button');
+        tempB.setAttribute('temp', temp);
+        tempB.setAttribute('onclick', "getClickCity(this.getAttribute('temp'))")
+        tempB.setAttribute('class', 'col-12')
+        tempB.innerHTML = temp.toUpperCase();
+        list.appendChild(tempB);
+    }
+}
+
+function getClickCity(city) {
+    getCity(city);
+}
 
 function setCurrentDay(response) {
     $('#currentCity').text(response.name);
@@ -21,10 +52,12 @@ function getCity(city) {
         console.log(response.coord.lon);
         console.log(response);
         setCurrentDay(response);
+        var icon = response.weather[0].icon;
+        var iconURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+        $('#icon').attr('src', iconURL);
         getWeek(response.coord.lat, response.coord.lon);
     });
 }
-
 
 function getWeek(lat, lon) {
     var requestUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ lon +'&units=imperial&exclude=hourly,minutely&appid=7563799c4abe9a09c92d23a0f1b3ecc8';
@@ -34,6 +67,24 @@ function getWeek(lat, lon) {
         method: 'GET',
     }).then(function (response) {
         console.log(response);
+        $('#cityUV').text(response.daily[0].uvi);
+        
+        setForecast(response);
     });
 }
 // getCity();
+$('#date').text(moment().format('M/DD/YYYY'))
+
+function setForecast(response) {
+    for (var i = 1; i < 6; i++) {
+        var unix = moment.unix(response.daily[i].dt);
+        var icon = response.daily[i].weather[0].icon;
+        var iconURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+        $('#date' + i).text(unix.format('M/DD/YYYY'));
+        $('#icon' + i).attr('src', iconURL);
+        $('#minTemp' + i).text(response.daily[i].temp.min);
+        $('#maxTemp' + i).text(response.daily[i].temp.max);
+        $('#Wind' + i).text(response.daily[i].wind_speed);
+        $('#Humid' + i).text(response.daily[i].humidity);
+    }
+}
